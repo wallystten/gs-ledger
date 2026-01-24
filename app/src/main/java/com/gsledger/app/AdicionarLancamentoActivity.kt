054b9d48be08845c3aeab249e268210237e8e052
wallystten <alfredosilvabarbosa29@gmail.com>
2026-01-24 08:36:35 -0300

    Create AdicionarLancamentoActivity.kt

---
+++ app/src/main/java/com/gsledger/app/AdicionarLancamentoActivity.kt
@@ -0,0 +1,12 @@
+package com.gsledger.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AdicionarLancamentoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adicionar_lancamento)
+    }
+}

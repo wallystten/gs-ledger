8c3a734c82c6db5b089bfecc5137601cbdc3489b
wallystten <alfredosilvabarbosa29@gmail.com>
2026-01-24 08:48:55 -0300

    Create AddTransactionActivity.kt

---
+++ app/src/main/java/com/gsledger/app/AddTransactionActivity.kt
@@ -0,0 +1,12 @@
+package com.gsledger.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_transaction)
+    }
+}

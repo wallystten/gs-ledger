614935ae3bce4b6e3a157957112df506e62b314e
wallystten <alfredosilvabarbosa29@gmail.com>
2026-02-01 03:22:11 -0300

    Create SobreActivity.kt

---
+++ app/src/main/java/com/gsledger/app/SobreActivity.kt
@@ -0,0 +1,11 @@
+package com.gsledger.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SobreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sobre)
+    }
+}
